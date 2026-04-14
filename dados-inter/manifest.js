e0a75bd8a6e559558a0dc9e971a8ad0a98e53bb8
wESLEY jUNIOR <wesley_jr40@msn.com>
2026-04-14 12:10:32 -0300

    feat: Add financial data for January to March 2026
    - Removed outdated Excel file `Extrato-01-01-2026-a-27-03-2026-CSV.xls`.
    - Introduced new JavaScript file `extrato-01-01-2026-a-31-03-2026-csv.js` containing detailed financial data for the period.
    - Created HTML file `extrato-01-01-2026-a-31-03-2026-csv.html` for displaying the financial summary and detailed tables.
    - Added CSV file `Extrato-01-01-2026-a-31-03-2026-CSV.csv` with transaction records for the specified period.

--- dados-inter/manifest.js
+++ dados-inter/manifest.js
@@ -1,20 +1,20 @@
 window.INTER_CARGAS = [
     {
-        "arquivoCsv": "Extrato-01-01-2024-a-31-12-2024-CSV.csv",
-        "dadosJs": "dados-inter/extrato-01-01-2024-a-31-12-2024-csv.js",
-        "periodo": "AGOSTO-2024 ate DEZEMBRO-2024",
-        "totalLancamentos": 104
+        arquivoCsv: 'Extrato-01-01-2024-a-31-12-2024-CSV.csv',
+        dadosJs: 'dados-inter/extrato-01-01-2024-a-31-12-2024-csv.js',
+        periodo: 'AGOSTO-2024 ate DEZEMBRO-2024',
+        totalLancamentos: 104,
     },
     {
-        "arquivoCsv": "Extrato-01-01-2025-a-31-12-2025-CSV.csv",
-        "dadosJs": "dados-inter/extrato-01-01-2025-a-31-12-2025-csv.js",
-        "periodo": "JANEIRO-2025 ate DEZEMBRO-2025",
-        "totalLancamentos": 279
+        arquivoCsv: 'Extrato-01-01-2025-a-31-12-2025-CSV.csv',
+        dadosJs: 'dados-inter/extrato-01-01-2025-a-31-12-2025-csv.js',
+        periodo: 'JANEIRO-2025 ate DEZEMBRO-2025',
+        totalLancamentos: 279,
     },
     {
-        "arquivoCsv": "Extrato-01-01-2026-a-27-03-2026-CSV.csv",
-        "dadosJs": "dados-inter/extrato-01-01-2026-a-27-03-2026-csv.js",
-        "periodo": "JANEIRO-2026 ate MARCO-2026",
-        "totalLancamentos": 74
-    }
+        arquivoCsv: 'Extrato-01-01-2026-a-31-03-2026-CSV.csv',
+        dadosJs: 'dados-inter/extrato-01-01-2026-a-31-03-2026-csv.js',
+        periodo: 'JANEIRO-2026 ate MARCO-2026',
+        totalLancamentos: 74,
+    },
 ];
